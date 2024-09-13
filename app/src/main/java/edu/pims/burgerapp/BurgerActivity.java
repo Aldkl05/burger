e1dfac2de61bffceb53d648ec3c90379cb6db014
Alish Dhakal <107578538+Aldkl05@users.noreply.github.com>
2024-09-13 09:46:50 +0545

    first commit

--- app/src/main/java/edu/pims/burgerapp/BurgerActivity.java
+++ app/src/main/java/edu/pims/burgerapp/BurgerActivity.java
@@ -1,5 +1,4 @@
 package edu.pims.burgerapp;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.util.TypedValue;
@@ -16,54 +15,35 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 public class BurgerActivity extends AppCompatActivity {
-
     RadioButton nonVegButton, vegButton;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        // EdgeToEdge.enable(this);
         setContentView(R.layout.activity_burger);
-
         nonVegButton = findViewById(R.id.nonVegRadioBtn);
         vegButton = findViewById(R.id.VegRadioBtn);
-
-
-
-
         nonVegButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FragmentManager fragmentManager=getSupportFragmentManager();
                 NonVegBurgerFragment nonVegBurgerFragment=new NonVegBurgerFragment();
-
                 FragmentTransaction transaction=fragmentManager.beginTransaction();
                 transaction.replace(R.id.burgerFrame,nonVegBurgerFragment);
                // transaction.addToBackStack(null);
                 transaction.commit();
-
             }
         });
-
         vegButton.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
                 FragmentManager fragmentManager=getSupportFragmentManager();
                 VegBurgerFragment VegBurgerFragment=new VegBurgerFragment();
-
                 FragmentTransaction transaction=fragmentManager.beginTransaction();
                 transaction.replace(R.id.burgerFrame,VegBurgerFragment);
                 //transaction.addToBackStack(null);
                 transaction.commit();
-
             }
         });
-
-
-
     }
-
-
 }
